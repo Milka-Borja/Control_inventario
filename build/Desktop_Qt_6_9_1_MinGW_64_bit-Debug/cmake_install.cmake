6d96033de5366fc0ff4f21ca7c72c78227700e84
Milka Borja <milk4borj4o@gmail.com>
2025-07-27 10:42:02 -0500

    Errores resueltos, ya se unio guardar, mostrar y el login en el main

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/cmake_install.cmake
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Vergil-Sebas/Documents/Segundo/Progra/proyect/proyect/Proyecto_Qt/Control_inventario
+# Install script for directory: C:/Users/milk4/Documentos/Inventario_Proyecto
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/Vergil-Sebas/Documents/Segundo/Progra/proyect/proyect/Proyecto_Qt/Control_inventario/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Inventario_Proyecto.exe")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/milk4/Documentos/Inventario_Proyecto/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Inventario_Proyecto.exe")
   if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/Inventario_Proyecto.exe" AND
      NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/Inventario_Proyecto.exe")
     if(CMAKE_INSTALL_DO_STRIP)
@@ -62,6 +62,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Vergil-Sebas/Documents/Segundo/Progra/proyect/proyect/Proyecto_Qt/Control_inventario/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/milk4/Documentos/Inventario_Proyecto/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
